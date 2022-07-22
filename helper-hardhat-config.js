4450e8200e5546755c881b13d745dec414a67574
nicolasCinzer <nicolas.cinzer00@gmail.com>
2022-07-21 23:04:58 -0300

    Devchains and networkconfig

---
+++ helper-hardhat-config.js
@@ -0,0 +1,13 @@
+const networkConfig = {
+    4: {
+        name: 'rinkeby',
+        vrf: '0x6168499c0cFfCaCD319c818142124B7A15E857ab'
+    }
+}
+
+const developmentChains = ['localhost', 'hardhat']
+
+module.exports = {
+    networkConfig,
+    developmentChains
+}
