ab8ec140c83864978279af54c8fece7ee37f2803
nicolasCinzer <nicolas.cinzer00@gmail.com>
2022-07-21 23:04:38 -0300

    Added ENV Variables and Networks

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,7 +6,37 @@ require('hardhat-gas-reporter')
 require('hardhat-contract-sizer')
 require('dotenv').config()
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const RINKEBY_RPC = process.env.RINKEBY_RPC || 'https://eth-rinkeby'
+const PRIVATE_KEY = process.env.PRIVATE_KEY || '0xkey'
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || 'apikey'
+const COINMARKETCAP_API_KEY = process.env.CMC_API_KEY || 'apikey'
+
 module.exports = {
     solidity: '0.8.9',
+    defaultNetwork: 'hardhat',
+    networks: {
+        hardhat: {
+            chainId: 31337,
+            blockConfirmations: 1,
+        },
+        rinkeby: {
+            url: RINKEBY_RPC,
+            accounts: [PRIVATE_KEY],
+            chainId: 4,
+            blockConfirmations: 6,
+        },
+        localhost: {
+            url: 'http://127.0.0.1:8545/',
+            chainId: 31337,
+        },
+    },
+
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+        player: {
+            default: 1,
+        },
+    },
 }
